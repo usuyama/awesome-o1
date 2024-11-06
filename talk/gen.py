2dd51b2797eb76b4733f651dd6e57123e1fd2eb9
Sasha <srush.research@gmail.com>
2024-11-06 12:30:29 -0500

    .

--- talk/gen.py
+++ talk/gen.py
@@ -233,18 +233,33 @@ def draw_node(root: Node):
 
 
 def draw(
-    nodes, edges, rollout_edges, name, T=6, csize=0.2, lwidth=0.5, draw_final=True
+    nodes,
+    edges,
+    rollout_edges,
+    name,
+    T=6,
+    csize=0.2,
+    lwidth=0.5,
+    draw_final=True,
+    final_opacity=1.0,
 ):
     ns = list(nodes)
     mag = False
+
+    def round(v):
+        if final_opacity < 1.0:
+            return int(v * 2) / 2
+        else:
+            return v
+
     if len(nodes[0]) == 3:
         mag = True
         nodes = np.array([(t, v) for (t, v, m) in ns if t <= T - 1])
         nodes_m = np.array([m for (t, v, m) in ns if t <= T - 1])
-        finaln = np.array([(t, int(v * 2) / 2) for (t, v, m) in ns if t == T])
+        finaln = np.array([(t, round(v)) for (t, v, m) in ns if t == T])
     else:
         nodes = np.array([(t, v) for (t, v) in ns if t <= T - 1])
-        finaln = np.array([(t, int(v * 2) / 2) for (t, v) in ns if t == T])
+        finaln = np.array([(t, round(v)) for (t, v) in ns if t == T])
     edges = np.array(edges)
     redges = np.array(rollout_edges)
 
@@ -259,7 +274,7 @@ def draw(
     if finaln.shape[0] > 0:
         pts = tx.to_P2(finaln)
         y = c.translate_by(pts)
-        y = y.concat().fill_color(c1).fill_opacity(0.2)
+        y = y.concat().fill_color(c1).fill_opacity(final_opacity)
     else:
         y = empty()
 
@@ -274,26 +289,31 @@ def draw(
         .fill_color("darkgrey")
     )
     lines2 = (
-        Path.from_points([tx.to_P2(redges[:, 0]), tx.to_P2(redges[:, 1])], False)
-        .stroke()
-        .line_width(lwidth)
+        (
+            Path.from_points([tx.to_P2(redges[:, 0]), tx.to_P2(redges[:, 1])], False)
+            .stroke()
+            .line_width(lwidth)
+            .line_color(c2)
+            + c.scale(0.2).translate_by(tx.to_P2(redges[:, 1]))
+        )
         .line_color(c2)
+        .fill_color(c2)
     )
     if draw_final:
         final = (
-            rectangle(0.3, 1)
+            rectangle(0.6, 1)
             .line_width(lwidth)
             .translate(T, 0)
             .fill_color(c3)
-            .fill_opacity(0.5)
-            .line_width(0)
+            .fill_opacity(0.7)
+            .line_width(0.05)
         )
     else:
         final = empty()
     base = (
-        lines.concat()
+        final
+        + lines.concat()
         + lines2.concat()
-        + final
         + z.concat().fill_color("white").line_width(0)
         + z.concat()
         + y
@@ -305,22 +325,130 @@ def draw(
             .align_l()
             .translate(-0.5, 0)
             .fill_color("#faf0e6")
-            .line_width(0)
+            .line_width(0.4)
             + base
         )
         base2.render(name, 512, draw_height=250)
     return base
 
 
+def make_background(d, minh=2):
+    w = d.get_envelope().width
+    h = d.get_envelope().height
+    r = (
+        rectangle(w + 1, max(h, minh))
+        .align_l()
+        .translate(-0.5, 0)
+        .fill_color("#faf0e6")
+        .line_width(0.2)
+    )
+    return r + d
+
+
+def make_tree():
+    # Create the root node
+    root = Node([], 0, 0, 0, 0, None)
+
+    # Layer 1
+    root.nodes = [Node([], 1.5, 0, 0, 1, root), Node([], -0.5, 0, 0, 1, root)]
+
+    # Layer 2
+    root.nodes[0].nodes = [
+        Node([], root.nodes[0].val + 0.8, 0, 0, 2, root.nodes[0]),
+        Node([], root.nodes[0].val - 0.3, 0, 0, 2, root.nodes[0]),
+    ]
+    root.nodes[1].nodes = [
+        Node([], root.nodes[1].val + 0.2, 0, 0, 2, root.nodes[1]),
+    ]
+
+    # Layer 3
+    chosen_path = random.choice(root.nodes)
+    for node in root.nodes:
+        for child in node.nodes:
+            if node == chosen_path:
+                child.nodes = [
+                    Node([], child.val + random.uniform(0.1, 0.5), 0, 0, 3, child),
+                    Node([], child.val - random.uniform(0.1, 0.5), 0, 0, 3, child),
+                ]
+            else:
+                child.nodes = []
+
+    # Layer 4
+    chosen_child = random.choice(chosen_path.nodes)
+    for child in chosen_path.nodes:
+        for grandchild in child.nodes:
+            if child == chosen_child:
+                grandchild.nodes = [
+                    Node(
+                        [],
+                        grandchild.val + random.uniform(0.05, 0.3),
+                        0,
+                        0,
+                        4,
+                        grandchild,
+                    ),
+                    Node(
+                        [],
+                        grandchild.val - random.uniform(0.05, 0.3),
+                        0,
+                        0,
+                        4,
+                        grandchild,
+                    ),
+                ]
+            else:
+                grandchild.nodes = []
+
+    # Layer 5
+    chosen_grandchild = random.choice(chosen_child.nodes)
+    chosen_grandchild.nodes.append(
+        Node(
+            [],
+            chosen_grandchild.val + random.uniform(0.05, 0.3),
+            0,
+            0,
+            5,
+            chosen_grandchild,
+        )
+    )
+
+    return root
+
+
+def linearize_tree(root: Node) -> Node:
+    def dfs(node):
+        if not node.nodes:
+            return [node]
+
+        result = [node]
+        for child in node.nodes:
+            result.extend(dfs(child))
+        return result
+
+    flattened = dfs(root)
+    for i in range(len(flattened) - 1):
+        flattened[i].nodes = [flattened[i + 1]]
+    flattened[-1].nodes = []
+    return flattened[0]
+
+
+random.seed(0)
+
+tree = make_tree()
+draw(*draw_node(tree), "images/search.png", T=5)
+draw(*draw_node(linearize_tree(tree)), "images/search2.png", T=5)
+
+
 T = 10
 root = Node([], 0, 0, 0, 0, None)
 list(mcts_step(root, T=T))
+random.seed(0)
 # for i in range(0, 5, 5):
 for i in range(0, 100, 5):
     for j, path in enumerate(mcts_step(root, T=T)):
         d = (
-            rectangle(T + 1, 7).line_width(0).align_l().translate(-0.5, 0)
-            + draw(*draw_node(root), "", T=T, csize=0.07)
+            # rectangle(T + 1, 7).line_width(0).align_l().translate(-0.5, 0)
+            draw(*draw_node(root), "", T=T, csize=0.07)
             + (
                 draw(
                     *draw_node(path), "", csize=0.07, T=T, draw_final=False
@@ -329,11 +457,12 @@ for i in range(0, 100, 5):
                 else empty()
             )
         )
+        d = make_background(d, 7)
         d.render(f"images/mcts{i + j:03}.png", 512, draw_height=100)
 
-vcat([draw(*make_chain(partial(rwalk1, d=1)), "x") for _ in range(5)]).render(
-    "images/reject1.png", 512
-)
+vcat(
+    [make_background(draw(*make_chain(partial(rwalk1, d=1)), "x")) for _ in range(5)]
+).render("images/reject1.png", 512, draw_height=250)
 
 
 draw(*make_chain(expand), "images/expand.png")
@@ -352,18 +481,52 @@ draw(
     *multi([make_chain(partial(rwalk1, d=1)) for _ in range(10)]),
     "images/bwalk.png",
     draw_final=False,
+    final_opacity=0.2,
+)
+
+random.seed(0)
+draw(
+    *multi(
+        [
+            (n, e, r)
+            for _ in range(10)
+            for n, e, r in [make_chain(partial(rwalk1, d=1))]
+            if -0.5 < n[-1][-1] < 0.5
+        ]
+    ),
+    "images/rejectb.png",
+    draw_final=True,
+)
+
+random.seed(0)
+draw(
+    *multi(
+        [
+            (n, e, r)
+            for _ in range(10)
+            for n, e, r in [make_chain(partial(rwalk1, d=1))]
+            if -0.5 < n[-1][-1] < 0.5
+        ]
+    ),
+    "images/rejectb.png",
+    draw_final=True,
 )
 
 draw(
-    *multi([make_chain(partial(rwalk1, d=1)) for _ in range(10)]),
-    "images/reject.png",
+    *multi([make_chain(partial(rwalk1, d=1)) for _ in range(100)]),
+    "images/all.png",
     draw_final=True,
 )
+
 
 T = 6
 draw(*make_beam(partial(rwalk, d=1), end=T + 1), "images/beam.png", draw_final=False)
 
-draw(*make_beam(partial(rwalk, d=1), beam=4, end=T - 1), "images/beamroll.png")
+
+for i in range(1, 5):
+    random.seed(2)
+    draw(*make_beam(partial(rwalk, d=2), beam=4, end=i + 1), f"images/beamroll{i}.png")
+
 
 draw(
     *make_beam(partial(rwalk, d=1), beam=4, end=T - 1, dorollout=False),
